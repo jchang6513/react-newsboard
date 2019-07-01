48d1250051f2a834063c86e349118c585dabb9a3
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-07-01 23:43:17 +0800

    render news with grid layout

--- src/actions.tsx
+++ src/actions.tsx
@@ -1,17 +1,29 @@
 import axios from "axios";
-import NewsFactory from "./NewsFactory";
+import NewsFactory, { NewsType } from "./NewsFactory";
 
+export interface GetNewsCallbacks {
+  onSuccess: (news: NewsType[]) => void;
+  onError: (err: Error) => void;
+}
 const apiKey: string = process.env.REACT_APP_NEWS_KEY;
-export function getNews(page: number) {
+export function getNews(
+  page: number,
+  callbacks: GetNewsCallbacks
+): Promise<void> {
   const body = {
     country: "tw",
+    category: "business",
     apiKey: apiKey,
-    page: page
+    page: page,
+    pageSize: 10
   };
-  axios
+  return axios
     .get("https://newsapi.org/v2/top-headlines", { params: body })
-    .then(response =>
-      console.log(NewsFactory.createNewsArrayFromNet(response.data.articles))
-    )
-    .catch(err => console.log(err));
+    .then(response => {
+      const news = NewsFactory.createNewsArrayFromNet(response.data.articles);
+      callbacks.onSuccess(news);
+    })
+    .catch(err => {
+      callbacks.onError(err);
+    });
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,67 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { getNews } from "./actions";
-import "./styles.css";
+import { getNews, GetNewsCallbacks } from "./actions";
+import { NewsType } from "./NewsFactory";
 
-export default class App extends React.Component {
-  componentDidUpdate() {}
+import "./styles.scss";
+
+interface AppStates {
+  currentPage: number;
+  newsArr: NewsType[];
+}
+export default class App extends React.Component<{}, AppStates> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      newsArr: []
+    };
+  }
+  componentDidMount() {
+    const { currentPage } = this.state;
+    const callbacks: GetNewsCallbacks = {
+      onSuccess: (newsArr: NewsType[]) => {
+        this.setState({ newsArr });
+      },
+      onError: err => {
+        console.log(err);
+      }
+    };
+    getNews(currentPage, callbacks);
+    document.addEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = () => {
+    const { currentPage } = this.state;
+    const isBottom =
+      document.getElementById("App").getBoundingClientRect().bottom <=
+      window.innerHeight;
+    if (isBottom) {
+      const callbacks: GetNewsCallbacks = {
+        onSuccess: (newsArr: NewsType[]) => {
+          const { newsArr: prevNews } = this.state;
+          this.setState({ newsArr: prevNews.concat(newsArr) });
+        },
+        onError: err => {
+          console.log(err);
+        }
+      };
+      getNews(currentPage + 1, callbacks);
+      this.setState({
+        currentPage: currentPage + 1
+      });
+    }
+  };
   render(): JSX.Element {
-    getNews(1);
+    const { newsArr } = this.state;
+
     return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <div id="App" onScroll={el => console.log("?")}>
+        {newsArr.map(news => (
+          <div className="news-grid" key={news.url}>
+            <img src={news.urlToImage} alt="" />
+            <p className="news-title">{news.title}</p>
+          </div>
+        ))}
       </div>
     );
   }
