5bee0dfca7e1207e6c51dd958b94eb753be0ec39
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-07-01 21:21:48 +0800

    change App component as a state component

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import { render } from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
