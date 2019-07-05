4bd668462447145829701eaf386b7f1abff224a2
Yi-Wei Chang <jchang6513@gmail.com>
2019-07-05 22:21:42 +0800

    add ts rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
 import { getNews, GetNewsCallbacks } from "./actions";
 import { NewsType } from "./NewsFactory";
