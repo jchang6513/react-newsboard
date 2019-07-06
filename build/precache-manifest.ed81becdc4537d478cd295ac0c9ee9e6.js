d9bc7fb03cbadc5fc291d8d9c01f3bc5d93d6d7c
Yi-Wei Chang <jchang6513@gmail.com>
2019-07-06 15:56:53 +0800

    build

---
+++ build/precache-manifest.ed81becdc4537d478cd295ac0c9ee9e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6eb08efcb7a21fec2cd3",
+    "url": "/static/js/main.6eb08efc.chunk.js"
+  },
+  {
+    "revision": "c096b11d5fcee206401e",
+    "url": "/static/js/1.c096b11d.chunk.js"
+  },
+  {
+    "revision": "6eb08efcb7a21fec2cd3",
+    "url": "/static/css/main.1275c8fa.chunk.css"
+  },
+  {
+    "revision": "4939b050a68ab5bfe909f95ee3ebae26",
+    "url": "/index.html"
+  }
+];
