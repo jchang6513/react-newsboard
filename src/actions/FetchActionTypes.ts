7d565d31e8e70845ba01f734c99de26ba075c606
Yi-Wei Chang <jchang6513@gmail.com>
2019-07-13 15:35:46 +0800

    remove non-use file

--- src/actions/FetchActionTypes.ts
+++
@@ -1,5 +0,0 @@
-export enum FetchActionTypes {
-  LOAD_TOP_NEWS_START = 'Fetch#LoadTopNewsStart',
-  LOAD_TOP_NEWS_SUCCESS = 'Fetch#LoadTopNewsSuccess',
-  LOAD_TOP_NEWS_FAIL = 'Fetch#LoadTopNewsFail',
-}
